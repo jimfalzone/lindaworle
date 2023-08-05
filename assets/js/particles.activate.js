396b290d637a5bc62e1dcf73558fda968b56ff5a
Jim Falzone <54333603+jimfalzone@users.noreply.github.com>
2023-08-04 22:47:03 -0400

    add testimonials to garden page

--- assets/js/particles.activate.js
+++ assets/js/particles.activate.js
@@ -73,24 +73,24 @@ particlesJS("particles-js", {
 
 );
 var count_particles,
-stats,
-update;
-stats=new Stats;
-stats.setMode(0);
-stats.domElement.style.position='absolute';
-stats.domElement.style.left='0px';
-stats.domElement.style.top='0px';
-document.body.appendChild(stats.domElement);
-count_particles=document.querySelector('.js-count-particles');
-update=function() {
-    stats.begin();
-    stats.end();
-    if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-        count_particles.innerText=window.pJSDom[0].pJS.particles.array.length;
-    }
-    requestAnimationFrame(update);
-}
 
-;
-requestAnimationFrame(update);
-;
+update;
+
+// stats.setMode(0);
+// stats.domElement.style.position='absolute';
+// stats.domElement.style.left='0px';
+// stats.domElement.style.top='0px';
+// document.body.appendChild(stats.domElement);
+// count_particles=document.querySelector('.js-count-particles');
+// update=function() {
+//     stats.begin();
+//     stats.end();
+//     if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+//         count_particles.innerText=window.pJSDom[0].pJS.particles.array.length;
+//     }
+//     requestAnimationFrame(update);
+// }
+
+// ;
+// requestAnimationFrame(update);
+// ;
